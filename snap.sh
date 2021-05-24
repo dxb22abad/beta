7470ac412deff4212ff9329086726ba4c9c7f034
dxb22abad <dxb22abad@gmail.com>
2021-05-24 03:18:48 -0400

    beta

--- snap.sh
+++ snap.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 date
 cd /root/snap2html/DiogenesList-master/
-python3 /root/snap2html/DiogenesList-master/diogeneslist.py /root/rclone/00 index
-sed -i "s#\[LINK ROOT\]#https://media.dxz.plus#g"  /root/snap2html/DiogenesList-master/index.html
+python3 /root/snap2html/DiogenesList-master/diogeneslist.py /root/rclone/beta index
+sed -i "s#\[LINK ROOT\]#https://betadata.dxz.plus/0\:#g"  /root/snap2html/DiogenesList-master/index.html
 sed -i "s#\[LINK PROTOCOL\]##g"  /root/snap2html/DiogenesList-master/index.html
 sed -i "s#\[SOURCE ROOT\]##g"  /root/snap2html/DiogenesList-master/index.html
 sed -i "s#\\\\\\\root\\\\\\\rclone\\\\\\\00##g"  /root/snap2html/DiogenesList-master/index.html
@@ -13,5 +13,5 @@ cp /root/snap2html/DiogenesList-master/index.html /root/snapGit/
 cd /root/snapGit
 git add .
 git commit -m 'snap2htmlDailyUpdate'
-git push -u origin master
+git push -u beta master
 date
