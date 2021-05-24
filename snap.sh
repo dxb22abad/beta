a94b583d160b623d142bc4b3feeb6e580f6695a6
dxb22abad <dxb22abad@gmail.com>
2021-05-24 06:21:46 -0400

    beta

--- snap.sh
+++ snap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 date
 cd /root/snap2html/DiogenesList-master/
-python3 /root/snap2html/DiogenesList-master/diogeneslist.py /root/rclone/00 index
+python /root/snap2html/DiogenesList-master/diogeneslist.py /root/rclone/beta index
 sed -i "s#\[LINK ROOT\]#https://betadata.xb.workers.dev/0\:/#g"  /root/snap2html/DiogenesList-master/index.html
 sed -i "s#\[LINK PROTOCOL\]##g"  /root/snap2html/DiogenesList-master/index.html
 sed -i "s#\[SOURCE ROOT\]##g"  /root/snap2html/DiogenesList-master/index.html
 sed -i "s#\\\\\\\root\\\\\\\rclone\\\\\\\beta\\\\\\\##g"  /root/snap2html/DiogenesList-master/index.html
-sed -i "s#1>index#1>所有内容均来自公开分享_收藏自用_侵权联系https://t.me/dxb22abad(telegram)#g" /root/snap2html/DiogenesList-master/index.html
-sed -i "s#e>index#e>00盘_用法详见dxz.plus#g" /root/snap2html/DiogenesList-master/index.html
+sed -i "s#1>index#1>学习目的_侵权可联系https://t.me/dxb22abad(telegram)#g" /root/snap2html/DiogenesList-master/index.html
+sed -i "s#e>index#e>00盘参考dxz.plus#g" /root/snap2html/DiogenesList-master/index.html
 cp /root/snap2html/DiogenesList-master/index.html /root/snapGit/
 cd /root/snapGit
 git add .
